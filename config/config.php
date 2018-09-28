47aa776f601086eab2b43159dbdc0f01a8aee839
studio <test>
2018-09-28 10:04:09 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	config/config.php

--- config/config.php
+++ config/config.php
@@ -1,8 +1,8 @@
 <?php
 //config
 //test - file
-const SRV_BASE_PAHTH = '/var/www/test/';
-const BASE_PATH_LIB =  SRV_BASE_PAHTH . '/lib';
+    const SRV_BASE_PAHTH = '/var/www/test/';
+    const BASE_PATH_LIB =  SRV_BASE_PAHTH . '/lib';
 /* please enter config vars below */
 
 class Config {
